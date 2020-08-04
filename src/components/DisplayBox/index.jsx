7b0dc0cbc9b4a16a98ddf7b27afdcef748629e15
howevert <1413537501@qq.com>
2020-08-04 14:43:26 +0800

    [App ToDoList][david && kevin] feature: add display box

---
+++ src/components/DisplayBox/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DisplayBox extends React.Component {
+    render() {
+        return <div>
+            <span>1</span><input type="button" value="delete"></input>
+        </div>
+    }
+}
+
+export default DisplayBox
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import DisplayBox from '../DisplayBox'
 class InputField extends React.Component {
     render() {
         return <div>
             <input type="text"></input> <input type="button" value="add"></input>
+            <br>
+            </br>
+            <DisplayBox />
         </div>
     }
 }
