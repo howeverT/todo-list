c929063f968e6f4261f018a9bb1a7368cde82e48
howevert <1413537501@qq.com>
2020-08-04 14:39:14 +0800

    [App ToDoList][david && kevin] feature: add button in input field

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class InputField extends React.Component {
     render() {
         return <div>
-            <input type="text"></input>
+            <input type="text"></input> <input type="button" value="add"></input>
         </div>
     }
 }
