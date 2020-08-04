ce5d6c72cd65609f06ee5f6042681dd8fa58c0ac
howevert <1413537501@qq.com>
2020-08-04 14:35:57 +0800

    [App ToDoList][david && kevin] feature: add input field

---
+++ src/components/InputField/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InputField extends React.Component {
+    render() {
+        return <div>
+            <input type="text"></input>
+        </div>
+    }
+}
+
+export default InputField
