ea1f6158c59428c4f0465aa67722f1e7a572ff3b
howevert <1413537501@qq.com>
2020-08-04 14:51:43 +0800

    [App ToDoList][david && kevin] feature: add constructor in input field

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import DisplayBox from '../DisplayBox'
 class InputField extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { inputValue: ""};
+    }
     render() {
         return <div>
-            <input type="text"></input> <input type="button" value="add"></input>
+            <input type="text" value={this.state.inputValue}></input> <input type="button" value="add"></input>
             <br>
             </br>
             <DisplayBox />
