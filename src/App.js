281131014fe18307b82cd362c2c70f89e599a418
howevert <1413537501@qq.com>
2020-08-05 16:30:00 +0800

    [App ToDoList][david && kevin] refactor: change App from function to class

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import { HashRouter, Route } from 'react-router-dom'
 import Finished from './components/Finished';
 import TopMenu from './components/Menu'
 import { Spin } from 'antd';
-import store from './redux/store'
 import { connect } from 'react-redux';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <HashRouter>
-          <Spin spinning = {store.getState().loading} >
-            <TopMenu></TopMenu>
-            <Route exact path="/" component={ToDoList}></Route>
-            <Route path="/finished" component={Finished}></Route>
-          </Spin>
-        </HashRouter>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <HashRouter>
+            <Spin spinning = {this.props.loading} >
+              <TopMenu></TopMenu>
+              <Route exact path="/" component={ToDoList}></Route>
+              <Route path="/finished" component={Finished}></Route>
+            </Spin>
+          </HashRouter>
+        </header>
+      </div>
+    );
+  }
+  }
+  
 const mapStateToProps = state => {
   return { loading: state.loading }
 };
